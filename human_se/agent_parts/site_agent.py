a9e49162886c72d088c899900c3ecf29a3b1085a
BodleHG <20181588@edu.hanbat.ac.kr>
2023-08-05 22:53:38 +0900

    agent_parts fixed

--- human_se/agent_parts/site_agent.py
+++ human_se/agent_parts/site_agent.py
@@ -1,6 +1,6 @@
 from base.agent_model import AgentModel
 
 class SiteAgent(AgentModel):
-    def __init__(self, name, site):
+    def __init__(self, site_info):
         super().__init__()
-        self.agent = {"name": name, "site" : site}
+        self.agent = {"name": site_info["name"], "site" : site_info["site"]}
