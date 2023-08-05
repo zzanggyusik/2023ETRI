0c1a886c1fe22dca3f65d7cace3f446712ec7823
zzanggyusik <20171632@edu.hanbat.ac.kr>
2023-08-05 17:51:29 +0900

    config_upload

---
+++ Pyrexiasim_Master/config.py
@@ -0,0 +1,62 @@
+from enum import Enum
+
+class Init():
+    enmae = 'pyrexiasim'
+    simulation_time = 'REAL_TIME'
+    simulation_unit_time = 1
+    
+    model_manager_name = 'model_manager'
+    telegram_manager_name = 'telegram_manager'
+
+class SimulationPort():
+    humanInfoModel_start = "humanModel_start"
+    humanInfoModel_finish = "humanModel_finish"
+    
+    humanSimulation_start = "humanSimulation_start"
+    humanSimulation_finish = ""
+    #####################################################
+    partsModelHandler_start = "partsModelHandler_start"
+    partsModelHandler_finish = "partsModelHandler_finish"
+
+    partsModel_start = "partsModel_start"
+    partsModel_finish = "partsModel_finish"
+    
+class SimulationModelState():
+    IDLE = "IDLE"
+    PROCESS = "PROCESS"
+    FINISH = "FINISH"
+    
+class WorkerFlushConfig():
+    model_name = 'Flush'
+    
+    health_info_port = 'health_info'
+    flush_port = 'flush'
+    
+    IDLE = 'IDLE'
+    
+    
+class WorkerRemoveConfig():
+    typevar = 'ModelManager'
+    
+    model_start_port = 'remove_worker'
+    
+    IDLE = 'IDLE'
+    HANDLE = 'HANDLE'
+    
+class DBConfig():
+    ip = 'localhost'
+    port = 27017
+    id = ''
+    pwd = ''
+    
+    human_db_name = ['pyrexiasim']
+    site_db_name = ['env']
+    
+    human_info_collection = ['human_info']
+    site_info_collection = ['site_info']
+    
+    
+    #def get_human_info()
+    
+class TelegramConfig():
+    token = ''
