53462efe3837746b7de3782145f2dd6df0e8ca06
bodlehg <20181588@edu.hanbat.ac.kr>
2023-11-04 22:57:04 +0900

    PyrexiadSim Config 파일 생성, 시뮬레이션 엔진 및 모니터 모델 수정 완료

---
+++ PyrexiaDsim/config.py
@@ -0,0 +1,63 @@
+class PyrexiaDsimConfig():
+    # PATH
+    path= "./Simulation/simulation.py"
+    result_path= "./Result/"
+    
+    # Engine Setting
+    mode= "REAL_TIME",
+    # mode= "VIRTUAL_TIME",
+    time_step= 1
+    instance_number= 100
+    
+    engine_name= "pyrexiadsim_engine"
+    model_name= "monitor_model"
+    
+    ## Simulation Engine Input Port
+    monitor_start= "monitor_start"
+    IDLE= "IDLE"
+    
+class MonitorModelConfig():
+    # Model Name
+    model_name= "monitor_model"
+    
+    # Model State
+    IDLE= "IDLE"
+    PROCESSING= "monitoring"
+    
+    # Model Port
+    start= "monitor_start"
+    fin= "monitor_finish"
+    
+class ContainerGeneratorConfig():
+    image_name= "zzanggyusik/generator:latest"
+    get_container_name= "CONTAINER_NAME"
+    
+class MongoDBConfig():
+    host= "192.168.50.201"
+    port= 27017
+    
+class ZMQ_NetworkConfig():
+    """
+    Network Config between monitor_model and container_generator,
+    between container_generator and human_agent container. 
+    r: ROUTER, d: DEALER
+    """
+    
+    # Network Config between monitor_model and container_generator
+    monitor_r_host= "*"
+    monitor_r_port= 8888
+    
+    generator_d_host= "host.docker.internal"
+    generator_d_port= 8888
+    
+    # Network Config between container_generator and human_agent container
+    generator_r_host= "*"
+    generator_r_port= 9999
+    
+    agent_d_host= "host.docker.internal"
+    agent_d_port= 9999
+    
+    
+    
+    
+    
